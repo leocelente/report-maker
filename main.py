b059b2eab9f0146731a525e82267664a4fae7936
leocelente <leocelente@gmail.com>
2020-05-08 14:26:30 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+'''
+    Given an dict that represents the line structure of a log file.
+    Generates a group-named Regular Expression matches and extracts 
+    the parameters according to the dict
+
+    RegEx Named Grouping Reference: 
+    https://docs.python.org/3.8/howto/regex.html#non-capturing-and-named-groups
+'''
+
+import re
+
+
+def make_regex(structure, separator):
+    '''
+        Generates the RegEx given structure and separator
+    '''
+    regex = r""
+    for key in structure:
+        # uses Python Named Group RegEx Extension: ?P<group name>
+        regex = regex + \
+            (r"(?P<" + key + ">{" + structure[key]+"})") + separator
+    date_re = r"[\d]{1,2}\-[\d]{1,2}\-[\d]{4}"      # DD-MM-YYYY format date
+    integer_re = r"[\-]?[\d]+"                      # signed integer
+    float_re = r"[\-][\d]+\.[\d]+"                  # signed float
+    regex = regex.replace(r"{date}", date_re)
+    regex = regex.replace(r"{integer}", integer_re)
+    regex = regex.replace(r"{float}", float_re)
+    # replaces last separator with EOL
+    return regex[0:-1] + r'$'
+
+
+def extract_as_dict(line, structure, separator):
+    '''
+        Does the matching and returns the parameters dict
+    '''
+    my_re = make_regex(structure, separator)
+    match = re.match(my_re, line)
+    if match:
+        return match.groupdict()
+    else:
+        return None
+
+
+if __name__ == "__main__":
+
+    structure = {
+        "my_date": "date",
+        "my_int": "integer",
+        "my_float": "float"
+    }
+    separator = ";"
+    line = "1-12-2021;123;-123.456"
+    parameters = extract_as_dict(line, structure, separator)
+    if parameters:
+        print("Extracted Data:")
+        print(parameters)
+    else:
+        print("Failed to parse")
