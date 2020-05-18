a1dc81b5a03b558a4c98ea745ff224911bf6b63d
leocelente <leocelente@gmail.com>
2020-05-18 19:46:31 -0300

    added support for str

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 '''
     Given an dict that represents the line structure of a log file.
-    Generates a group-named Regular Expression matches and extracts 
+    Generates a group-named Regular Expression matches and extracts
     the parameters according to the dict
 
-    RegEx Named Grouping Reference: 
+    RegEx Named Grouping Reference:
     https://docs.python.org/3.8/howto/regex.html#non-capturing-and-named-groups
 '''
 
@@ -23,9 +23,11 @@ def make_regex(structure, separator):
     date_re = r"[\d]{1,2}\-[\d]{1,2}\-[\d]{4}"      # DD-MM-YYYY format date
     integer_re = r"[\-]?[\d]+"                      # signed integer
     float_re = r"[\-][\d]+\.[\d]+"                  # signed float
+    str_re = r"[\w]+"
     regex = regex.replace(r"{date}", date_re)
     regex = regex.replace(r"{integer}", integer_re)
     regex = regex.replace(r"{float}", float_re)
+    regex = regex.replace(r"{str}", str_re)
     # replaces last separator with EOL
     return regex[0:-1] + r'$'
 
@@ -42,18 +44,29 @@ def extract_as_dict(line, structure, separator):
         return None
 
 
-if __name__ == "__main__":
+def parse_file():
+    # construct structure with menu?
+    # read file
+    # populate lists for each data type
+    # generate statistics & graphs...
+    # ?? profit
+    pass
 
+
+if __name__ == "__main__":
+    filename = "test.log"
     structure = {
         "my_date": "date",
         "my_int": "integer",
-        "my_float": "float"
+        "my_float": "float",
+        "my_str": "str"
     }
     separator = ";"
-    line = "1-12-2021;123;-123.456"
+    line = "1-12-2021;123;-123.456;xa"
+
     parameters = extract_as_dict(line, structure, separator)
     if parameters:
-        print("Extracted Data:")
-        print(parameters)
-    else:
-        print("Failed to parse")
+            print("Extracted Data:")
+            print(parameters)
+        else:
+            print("Failed to parse")
